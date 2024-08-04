d16c1055893a78348b39e5c5c3adfca2722f8c12
jongho3165 <wkdwhdgh3166@naver.com>
2024-08-04 07:58:21 +0000

    commit

--- django/video/tube/settings.py
+++ django/video/tube/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s2z-tk2t2yvfqce(v*ld@@hga_7!h7_phz_!=2#3qejtsc0b4=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
