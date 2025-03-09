8a7bc6bd9f8f147778e971994d6b4fab9231ad42
jongho3165 <wkdwhdgh3166@naver.com>
2025-03-09 17:47:08 +0900

    change url

--- django/dic_ms/config/urls.py
+++ django/dic_ms/config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('search_engine/', include('search_engine.urls')),
+    path('', include('search_engine.urls')),
 ]
